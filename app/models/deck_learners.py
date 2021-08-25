eda8fd2d9d7a6720d8e3121eeeef6d2d88d36710
Dechon Ryan <dechon@tuta.io>
2021-08-25 10:38:20 -0700

    Fix typo in deck_learners

--- app/models/deck_learners.py
+++ app/models/deck_learners.py
@@ -2,10 +2,10 @@ from .db import db
 
 # joins table for decks and users
 deck_learners = db.Table('deck_learners',
-                         db.Colmumn('learner_id',
-                                    db.Integer,
-                                    db.ForeignKey('users.id'),
-                                    primary_key=True),
+                         db.Column('learner_id',
+                                   db.Integer,
+                                   db.ForeignKey('users.id'),
+                                   primary_key=True),
                          db.Column('deck_id',
                                    db.Integer,
                                    db.ForeignKey('decks.id'),
