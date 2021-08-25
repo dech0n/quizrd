03eed72e47570d3d4630d3e7cab6e85398613166
Dechon Ryan <dechon@tuta.io>
2021-08-25 11:12:56 -0700

    Change backref on User model to be 'owner'

---
+++ app/seeds/decks.py
@@ -0,0 +1,11 @@
+from app.models import db, Deck, User
+
+demo = User.query.get(1)
+marnie = User.query.get(2)
+bobbie = User.query.get(3)
+
+
+def seed_decks():
+    french_colors = Deck(
+        owner=
+    )
