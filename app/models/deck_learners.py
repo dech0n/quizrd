56f65a7bb1a764616a4751aafb99d74b78318291
Dechon Ryan <dechon@tuta.io>
2021-08-25 10:32:14 -0700

    Set up all models and relationships - untested.

---
+++ app/models/deck_learners.py
@@ -0,0 +1,13 @@
+from .db import db
+
+# joins table for decks and users
+deck_learners = db.Table('deck_learners',
+                         db.Colmumn('learner_id',
+                                    db.Integer,
+                                    db.ForeignKey('users.id'),
+                                    primary_key=True),
+                         db.Column('deck_id',
+                                   db.Integer,
+                                   db.ForeignKey('decks.id'),
+                                   primary_key=True)
+                         )
