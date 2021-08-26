aff174baa9a3f6f6ec68f3e6f074aa21ecaf1883
Dechon Ryan <dechon@tuta.io>
2021-08-26 16:37:22 -0700

    Render deck information in deck manager on homepage.

--- react-app/src/components/HomePage/DeckManagerTable.js
+++ react-app/src/components/HomePage/DeckManagerTable.js
@@ -1,22 +1,28 @@
 import React from 'react'
 
 function DeckManagerTable({ decks }) {
-    // consider <caption> and <colgroup> elements
     return (
         <ul className='deck-manager-list'>
-            {decks.map(deck => (
-                <li
-                    key={`${deck.id}-${deck.title}`}
-                    className='deck-manager-row'>
-                    <ul className='deck-manager-row-contents'>
-                        <li className='deck-info'>
-                            <h4>{deck.title}</h4>
-                            <p>{deck.description}</p>
-                            <p>Categories: ...</p>
-                        </li>
-                    </ul>
-                </li>
-            ))}
+            {decks.map(deck => {
+                const { categories } = deck
+                return (
+                    <li
+                        key={`${deck.id}-${deck.title}`}
+                        className='deck-manager-row'>
+                        <ul className='deck-manager-row-contents'>
+                            <li className='deck-info'>
+                                <h4>{deck.title}</h4>
+                                <p>{deck.description}</p>
+                                <p>
+                                    Categories: {categories.map(category => (
+                                        <span>{category.name}, </span>
+                                    ))}
+                                </p>
+                            </li>
+                        </ul>
+                    </li>
+                )
+            })}
         </ul>
     )
 }
