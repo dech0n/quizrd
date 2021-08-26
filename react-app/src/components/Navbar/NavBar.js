c2778c331ac08837243c2319d1e2e26d3b855c16
Dechon Ryan <dechon@tuta.io>
2021-08-25 20:01:59 -0700

    Move auth form to center of screen. Arrange navbar items.

--- react-app/src/components/Navbar/NavBar.js
+++ react-app/src/components/Navbar/NavBar.js
@@ -1,35 +1,36 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import LogoutButton from '../auth/LogoutButton';
+import './Navbar.css'
 
 const NavBar = () => {
   return (
     <nav className='navbar navbar-container'>
-      <div>'LOGO'</div>
-      <ul className='navlinks-container'>
-        <li>
+      <div id='logo'>[****LOGO****]</div>
+      <ul className='all-navlinks-container'>
+        <li className='navlink-container'>
           <NavLink to='/' exact={true} activeClassName='active' className='navbar navlink' id='home'>
             Home
           </NavLink>
         </li>
-        <li>
+        <li className='navlink-container'>
           <NavLink to='/login' exact={true} activeClassName='active' className='navbar navlink' id='login'>
             Login
           </NavLink>
         </li>
-        <li>
+        <li className='navlink-container'>
           <NavLink to='/sign-up' exact={true} activeClassName='active' className='navbar navlink' id='signup'>
             Sign Up
           </NavLink>
         </li>
-        <li>
+        {/* <li className='navlink-container'>
           <NavLink to='/users' exact={true} activeClassName='active' className='navbar navlink' id='users'>
             Users
           </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
+        </li> */}
+        <li className='navlink-container' id='logout-btn-container'>
+          <LogoutButton id='logout'/>
         </li>
       </ul>
     </nav>
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './auth.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +32,43 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <div className='form-container'>
+      <form className='auth-form' onSubmit={onLogin}>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div className='email-container auth-field-container'>
+          <label
+            className='email-label auth-label'
+            htmlFor='email'>Email</label>
+          <input
+            className='email-input auth-input'
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className='password-container auth-field-container'>
+          <label
+            className='password-label auth-label' htmlFor='password'>Password</label>
+          <input
+            className='password-input auth-input'
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+          <button
+            className='submit-btn auth-btn'
+            type='submit'>Login</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
