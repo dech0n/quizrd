2fc679d34205f4d2fbc07a5ab93379ed118767a0
Dechon Ryan <dechon@tuta.io>
2021-08-25 18:54:57 -0700

    Set global padding and margin to 0

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -5,25 +5,26 @@ import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
+    <nav className='navbar navbar-container'>
+      <div>'LOGO'</div>
+      <ul className='navlinks-container'>
         <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
+          <NavLink to='/' exact={true} activeClassName='active' className='navbar navlink' id='home'>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
+          <NavLink to='/login' exact={true} activeClassName='active' className='navbar navlink' id='login'>
             Login
           </NavLink>
         </li>
         <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          <NavLink to='/sign-up' exact={true} activeClassName='active' className='navbar navlink' id='signup'>
             Sign Up
           </NavLink>
         </li>
         <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+          <NavLink to='/users' exact={true} activeClassName='active' className='navbar navlink' id='users'>
             Users
           </NavLink>
         </li>
