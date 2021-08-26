68c30538cf151be7e362127083536f8075f7b71d
Dechon Ryan <dechon@tuta.io>
2021-08-25 17:24:59 -0700

    Create GET and DELETE API routes for single decks

--- app/api/deck_routes.py
+++ app/api/deck_routes.py
@@ -5,9 +5,10 @@ from flask_login import current_user, login_required
 
 deck_routes = Blueprint('decks', __name__)
 
-
+# TODO: add PUT method & logic
 @deck_routes.route('/', methods=['POST'])
-def decks():
+@login_required
+def new_deck():
     data = request.get_json()
     # POST data should look like this:
     # {
@@ -39,8 +40,21 @@ def decks():
             category = Category(name=category_data["name"])
             deck.categories.append(category)
             categories_for_db.append(category)
-
+        # get everything into the database
         db.session.add(deck)
         db.session.add_all(categories_for_db)
         db.session.commit()
-        return deck.to_dict()
+        return deck.to_dict()  # contains the new categories
+
+
+@deck_routes.route('/<int:id>', methods=['GET', 'DELETE'])
+def get_or_delete_deck(id):
+    deck = Deck.query.get(id)
+
+    if request.method == 'GET':
+        pass  # already got deck from db
+    elif request.method == 'DELETE':
+        db.session.delete(deck)
+        db.session.commit()
+
+    return deck.to_dict()
