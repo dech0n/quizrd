70b1236a84d7ba7465fb7611a6fcbd5f32103323
Dechon Ryan <dechon@tuta.io>
2021-08-27 08:31:34 -0700

    Add 'New Deck' button.

--- react-app/src/components/HomePage/HomePage.js
+++ react-app/src/components/HomePage/HomePage.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import FormManagerTable from './DeckManagerTable'
+import authenticate from '../../store/session'
+import DeckManagerTable from './DeckManagerTable'
 
 // TODO: add "edit photo" button for profile pic
 // TODO: make sure there's a default image for profile pics (and proper updates for User model)
 function HomePage() {
     const user = useSelector(state => state.session.user)
+    const dispatch = useDispatch()
 
-    // TODO: Add useEffect hook for the user -- hopefully will rerender when a deck is created
+    // TODO: click handler for New Deck button
+    // TODO: useEffect for rerender after New Deck created
 
     return user ? (
         <>
@@ -16,16 +19,16 @@ function HomePage() {
             <div className='deck-manager-page-container'>
                 <div className='deck-manager-header'>
                     <div className='user-info-container'>
-                        {/* profile pic, "Hello <username>!"  */}
+                        {/* profile pic, "Hello <username>!" Add # decks & # decks-learninig under username ?  */}
                         <img src={user.image} alt='profile pic' />
                         <p>Welcome back, {user.username}!</p>
                     </div>
                     <div className='new-deck-btn-container'>
-                        {/* new deck button */}
+                        <button>+ New Deck</button>
                     </div>
                 </div>
                 <div className='deck-manager-container'>
-                    <FormManagerTable decks={user.decks}/>
+                    <DeckManagerTable decks={user.decks}/>
                 </div>
             </div>
         </>
