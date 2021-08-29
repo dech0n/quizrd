32a46ccd805fec4c2b57b9df3350e3459626b5d4
Dechon Ryan <dechon@tuta.io>
2021-08-28 17:35:42 -0700

    Minor changes to homepage styling

--- react-app/src/components/HomePage/DeckManagerTable.js
+++ react-app/src/components/HomePage/DeckManagerTable.js
@@ -10,21 +10,20 @@ function DeckManagerTable({ decks, handleDelete }) {
                 return (
                     <li key={`${deck.id}-deck-row`}
                         className='deck-manager-row'>
+                            <Link to={'#'}>
                         <ul key={`${deck.id}-deck-contents`}
                             className='deck-manager-row-contents'>
                             <li key={`${deck.id}-deck-info`}
                                 className='deck-info'>
                                     {/* TODO: Link to Deck page */}
-                                <Link to={'#'}>
                                     <h4 key={`${deck.id}-deck-title`}
                                         className='deck-title'>{deck.title}</h4>
-                                </Link>
                                 <p key={`${deck.id}-deck-desc`}
                                     className='deck-description'>{deck.description}</p>
                                 <p key={`${deck.id}-deck-cats`}>
                                     Categories: {categories.map(category => (
                                         <span key={`${category.id}-catgry-name`}
-                                            className='category-name'>
+                                        className='category-name'>
                                             {/* TODO: Format this better */}
                                             • {category.name} </span>
                                     ))}
@@ -37,6 +36,7 @@ function DeckManagerTable({ decks, handleDelete }) {
                                         onClick={() => handleDelete(deck.id)}>Delete</button>
                             </li>
                         </ul>
+                                        </Link>
                     </li>
                 )
             })}
