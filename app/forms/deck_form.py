bb0ec0f380a09d05deea1c8fd153dfe829030f86
Dechon Ryan <dechon@tuta.io>
2021-08-30 14:34:24 -0700

    Add length validator to deck form.

--- app/forms/deck_form.py
+++ app/forms/deck_form.py
@@ -1,9 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class DeckForm(FlaskForm):
-    title = StringField('title', validators=[DataRequired()])
+    title = StringField('title', validators=[
+        DataRequired(),
+        Length(
+            min=3,
+            max=50,
+            message="Title must be 3-50 characters."
+        )
+        ])
     description = StringField('description')
     image = StringField('image')
