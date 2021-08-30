471aa411823eae89e6d71cf83002ec4e4d8d6c46
Dechon Ryan <dechon@tuta.io>
2021-08-30 13:31:15 -0700

    Create thunks for adding a single card to cards slice of state and adding all cards belonging to a specific deck.

--- react-app/src/store/cards.js
+++ react-app/src/store/cards.js
@@ -18,6 +18,34 @@ const remove = (cards) => ({
     cards
 })
 
+// THUNK ACTIONS
+// TODO: Implement error handling for each thunk
+
+// get a single card by pk
+const getOneCard = (cardId) => async (dispatch) => {
+    const res = await fetch(`/api/cards/${cardId}`)
+
+    if (res.ok) {
+        const card = await res.json()
+        dispatch(load([card])) // must be array for the reducer
+        // return card
+    }
+}
+
+// get all cards for a single deck
+const getDeckCards = (deckId) => async (dispatch) => {
+    const res = await fetch(`/api/cards/decks/${deckId}`)
+
+    if (res.ok) {
+        const {cards} = res.json()
+        dispatch(load(cards))
+        // return cards
+    }
+}
+// create a card
+
+// delete a single card by pk
+
 const initialState = {}
 export default function cardsReducer(state = initialState, {type, cards}) {
     // makes it easier to denote a single card vs. multiple cards
