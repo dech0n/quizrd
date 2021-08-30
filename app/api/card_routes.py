2ff2050da265f57fe31b57083fdf04cf4704c076
Dechon Ryan <dechon@tuta.io>
2021-08-30 11:07:10 -0700

    Import CardForm to card_routes

--- app/api/card_routes.py
+++ app/api/card_routes.py
@@ -1,7 +1,7 @@
 from app.api.auth_routes import validation_errors_to_error_messages
 from flask import Blueprint, jsonify, session, request
 from app.models import db, Deck, Card
-# from app.forms import CardForm
+from app.forms import CardForm
 from flask_login import current_user, login_required
 
 
