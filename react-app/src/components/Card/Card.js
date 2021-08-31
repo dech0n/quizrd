e8832ee58de556b7b47ca707be38d493285715f1
Dechon Ryan <dechon@tuta.io>
2021-08-31 16:45:56 -0700

    Add flashcard flip logic.

--- react-app/src/components/Card/Card.js
+++ react-app/src/components/Card/Card.js
@@ -1,17 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getOneCard } from '../../store/cards'
 
-function Card({cardId}) {
+function Card({ cardId }) {
     const dispatch = useDispatch()
     const card = useSelector(state => state.cards[cardId])
+    const [flipCard, setFlipCard] = useState(true)
 
     useEffect(() => {
         dispatch(getOneCard(cardId))
     }, [dispatch, cardId])
 
     return (
-        <h1>The Current Flashcard</h1>
+        <>
+            <h1>The Current Flashcard</h1>
+            <div id='study-card'>
+                {flipCard ?
+                    <div
+                        id='study-card-front'
+                        onClick={() => setFlipCard(!flipCard)}
+                    >{card.front_text}</div>
+                    : // else
+                    <div
+                        id='study-card-back'
+                        onClick={() => setFlipCard(!flipCard)}
+                    >{card.back_text}</div>
+                }
+
+            </div>
+        </>
     )
 }
 
