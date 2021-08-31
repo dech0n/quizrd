099297f4bcfb7836da803a73349f20890ed0995c
Dechon Ryan <dechon@tuta.io>
2021-08-31 13:40:03 -0700

    Clarified deck form error message.

--- app/forms/deck_form.py
+++ app/forms/deck_form.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length
 
 class DeckForm(FlaskForm):
     title = StringField('title', validators=[
-        DataRequired(message="This deck needs a title."),
+        DataRequired(message="Decks need a title."),
         Length(
             min=3,
             max=50,
