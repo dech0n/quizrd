5802069789517cb5a7814072257c48011873c772
Dechon Ryan <dechon@tuta.io>
2021-08-31 15:55:27 -0700

    Add plan for flashcard render on study page.

--- react-app/src/components/Deck/StudyDeck.js
+++ react-app/src/components/Deck/StudyDeck.js
@@ -8,7 +8,7 @@ function StudyDeck() {
     const dispatch = useDispatch()
     const { deckId } = useParams()
     const user = useSelector(state => state.session.user)
-    const deck = useSelector(state => state.decks)
+    const deck = useSelector(state => state.decks[deckId])
     const cards = Object.values(useSelector(state => state.cards))
 
     // console.log('*** STUDY DECK ***', deck)
@@ -22,9 +22,23 @@ function StudyDeck() {
     return deck && cards ? (
         <>
             <h1>Study Deck Page</h1>
+            {/* Add deck tile (and desc?) somewhere */}
             <div className='deck-study-actions'>
                 {user ? <button type="button">Edit</button> : null} {/* replace with Edit Deck Form Modal */}
-                <button type="button">Preview Deck</button>
+                <button type="button">Preview Deck</button> {/* Brings up modal to preview both sides of every card */}
+            </div>
+            <div id='deck-study-flashcards'>
+                {/***   use a separate component here ???
+                 * useState above for cardId,
+                 * send to flashcard component as prop,
+                 * increment/decrement (setCardId) every time arrow is clicked,
+                 * set conditional render:
+                 *   - previous arrow: cardId > 1
+                 *   - next arrow: cardId < cards.length
+                 * get initial cardId from useParams   ***/}
+                {/* "Previous" button here */}
+                {/* render one card at a time */}
+                {/* "Next" button here */}
             </div>
         </>
     ) : (
