1336e205b50a9e35d30e557ad77cef3bbf6669e3
Dechon Ryan <dechon@tuta.io>
2021-08-31 11:16:36 -0700

    Grab relevant cards and decks from their slices of state.

--- react-app/src/components/Deck/StudyDeck.js
+++ react-app/src/components/Deck/StudyDeck.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import { getDeckCards } from '../../store/cards';
+import { getOneDeck } from '../../store/decks';
 
 function StudyDeck() {
+    const dispatch = useDispatch()
+    const { deckId } = useParams()
+    const user = useSelector(state => state.session.user)
+    const deck = useSelector(state => state.decks)
+    const cards = Object.values(useSelector(state => state.cards))
+
+    // console.log('*** STUDY DECK ***', deck)
+    // console.log('*** STUDY CARDS ***', cards)
+
+    useEffect(() => {
+        dispatch(getOneDeck(deckId))
+        dispatch(getDeckCards(deckId))
+    }, [dispatch, deckId])
+
     return (
         <h1>Study Deck Page</h1>
     )
