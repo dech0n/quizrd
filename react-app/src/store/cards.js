255cf6e80a9ae621257306320125a99f968baacd
Dechon Ryan <dechon@tuta.io>
2021-08-31 20:32:34 -0700

    Fix typo in thunk fetch

--- react-app/src/store/cards.js
+++ react-app/src/store/cards.js
@@ -53,7 +53,7 @@ export const getDeckCards = (deckId) => async (dispatch) => {
 
 // create a card
 export const createCard = (cardData) => async (dispatch) => {
-    const res = await fetch(`/cards`, {
+    const res = await fetch(`/api/cards`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
