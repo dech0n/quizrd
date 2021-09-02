8accec72bc66b4b39142f82ef80de00d8a2483bf
Dechon Ryan <dechon@tuta.io>
2021-09-02 12:46:05 -0700

    Render all decks on splash page. Make each deck into a link to the study page.

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { getAllDecks } from '../../store/decks'
 
 function SplashPage() {
-    return (
-        <h1>Splash Page</h1>
+    const dispatch = useDispatch()
+    const decks = useSelector(state => Object.values(state.decks))
+
+    useEffect(() => {
+        dispatch(getAllDecks())
+    })
+    return decks ? (
+        <>
+            <h1>Splash Page</h1>
+            {/* Logo Image */}
+            {/* Quick blurb about website */}
+            <ul>
+                {decks.map(deck => (
+                    <Link to={`/decks/${deck.id}/study`}>
+                        <li className='splash-deck-tile'>
+                            <h4>{deck.title}</h4>
+                            <p>{deck.description}</p>
+                        </li>
+                    </Link>
+                ))}
+            </ul>
+        </>
+    ) : (
+        <h1>Loading...</h1>
     )
 }
 
