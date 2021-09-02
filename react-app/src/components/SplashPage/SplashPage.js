67da59e9ef47dcc879ca5c0b21d25f35f3308781
Dechon Ryan <dechon@tuta.io>
2021-09-02 15:06:08 -0700

    More styling. Add blurb about basic features.

--- react-app/src/components/SplashPage/SplashPage.js
+++ react-app/src/components/SplashPage/SplashPage.js
@@ -12,13 +12,20 @@ function SplashPage() {
         dispatch(getAllDecks())
     })
     return decks ? (
-        <>
-            <h1>Splash Page</h1>
+        <div id='splash-container'>
             {/* Logo Image */}
+            <h1>Features</h1>
             {/* Quick blurb about website */}
+
+            <h3>Create great flashcards</h3>
+            <p>Create custom decks with exactly what you want to learn.</p>
+            <h3>Find great flashcards</h3>
+            <p>Browse decks created by other avid learners like you.</p>
+            <h3>Share your decks</h3>
+            <p>Post a link to your class discussion, or tweet it to the rest of the world!</p>
             <div id='splash-decks-container'>
             <ul id='splash-decks-list'>
-                {decks.map(deck => (
+                {decks && decks.map(deck => (
                     <Link to={`/decks/${deck.id}/study`}>
                         <li className='splash-deck-tile'>
                             <h4>{deck.title}</h4>
@@ -28,7 +35,7 @@ function SplashPage() {
                 ))}
             </ul>
             </div>
-        </>
+        </div>
     ) : (
         <h1>Loading...</h1>
     )
